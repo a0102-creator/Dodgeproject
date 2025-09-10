cd180f0f6b3e4f999c20426016b190548f91dd28
munument1 <seungbinoh1021@gmail.com>
2025-09-10 10:03:59 +0900

    Initial commit

---
+++ Assets/02.Scripts/Player.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    float hAxis;
+    float vAxis;
+    bool wDown;
+
+    Vector3 moveVec;
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+
+    void Update()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
+
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+        anim.SetBool("isWalk", wDown);
+
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    public void Die()
+    {
+        //자신의 게임 오브젝트를 비활성화
+        gameObject.SetActive(false);
+    }
+}
