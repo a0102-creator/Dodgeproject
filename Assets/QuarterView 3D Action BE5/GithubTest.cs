84d806a6c15ac77865b257f14ad927b90d28e981
munument1 <seungbinoh1021@gmail.com>
2025-09-10 10:23:22 +0900

    202509101022_init
    연동 테스트

---
+++ Assets/QuarterView 3D Action BE5/GithubTest.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 안녕하세요
+        // 지금은 깃허브 연동 테스트 중입니다
+        // 연동이 잘 된다면 깃허브 공간에도 해당 내용이 보이겠죠
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
