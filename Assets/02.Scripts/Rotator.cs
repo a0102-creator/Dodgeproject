35ca4f33c08c10927c439c0f019113a3e35ea7fa
munument1 <seungbinoh1021@gmail.com>
2025-09-11 16:34:03 +0900

    202509111633
    국제직업전문학교 닷지게임 모작

--- Assets/02.Scripts/Rotator.cs
+++ Assets/02.Scripts/Rotator.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
-    public float rotationSpeed = 60f;
+    public float rotationSpeed = 45f;
 
     void Update() {
         transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
